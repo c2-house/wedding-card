0b2d8747d24db76f76b9be4a16faad56ea4d66ea
eunchae <kec0130@gmail.com>
2023-05-07 23:14:07 +0900

    add address

--- src/routes/Invitation/components/Location.tsx
+++ src/routes/Invitation/components/Location.tsx
@@ -1,4 +1,8 @@
 import { useEffect, useRef } from 'react'
+import styles from './location.module.scss'
+
+import kakaoIcon from 'assets/icons/kakao_map.png'
+import naverIcon from 'assets/icons/naver_map.png'
 
 const Location = () => {
   const mapElement = useRef<HTMLDivElement>(null)
@@ -29,7 +33,24 @@ const Location = () => {
   return (
     <section>
       <h2>LOCATION</h2>
+      <h3 className={styles.location}>아펠가모 공덕</h3>
+      {/* TODO: 예식장 이미지 캐러셀 */}
       <div ref={mapElement} style={{ width: '100%', height: '200px' }} />
+      <div className={styles.address}>
+        <p>
+          서울특별시 마포구 마포대로 92
+          <br />
+          효성해링턴스퀘어 B동 7층 마리에홀
+        </p>
+        <div className={styles.mapIcons}>
+          <a href='https://naver.me/GSUnQUY2' target='_blank' rel='noreferrer'>
+            <img src={naverIcon} alt='네이버 지도' className={styles.naver} />
+          </a>
+          <a href='http://kko.to/_ZFVcBHhXC' target='_blank' rel='noreferrer'>
+            <img src={kakaoIcon} alt='카카오 지도' />
+          </a>
+        </div>
+      </div>
     </section>
   )
 }
