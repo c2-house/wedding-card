b3e8e97d45a9f1ba64119fa67f839f1ace42f7b1
eunchae <kec0130@gmail.com>
2023-05-09 23:29:59 +0900

    add gallery

--- src/components/ImageGrid/index.tsx
+++ src/components/ImageGrid/index.tsx
@@ -1,37 +1,20 @@
 import { MouseEvent } from 'react'
-import cx from 'classnames'
 
-import { BASE_URL } from 'constants/images'
 import styles from './imageGrid.module.scss'
 
 export interface IImageGridProps {
   imageList: string[]
-  className?: string
-  keyPrefix: string
-  directory: string
-  inModal?: boolean
   handleImageClick?: (e: MouseEvent) => void
 }
 
-const ImageGrid = ({
-  imageList,
-  className,
-  keyPrefix,
-  directory,
-  inModal = false,
-  handleImageClick,
-}: IImageGridProps) => {
+const ImageGrid = ({ imageList, handleImageClick }: IImageGridProps) => {
   return (
-    <ul className={cx(styles.imageList, styles[className || ''])}>
+    <ul className={styles.imageList}>
       {imageList.map((image) => (
-        <li key={`${keyPrefix}-${image}`}>
-          {inModal ? (
-            <button type='button' onClick={handleImageClick}>
-              <img src={`/images/${image}.jpeg`} alt={image} />
-            </button>
-          ) : (
-            <img src={`${BASE_URL}/${directory}/${image}.jpeg`} alt={image} />
-          )}
+        <li key={image}>
+          <button type='button' onClick={handleImageClick}>
+            <img src={image} alt={image} />
+          </button>
         </li>
       ))}
     </ul>
--- src/routes/Gallery/index.tsx
+++ src/routes/Gallery/index.tsx
@@ -1,7 +1,63 @@
+import gif01 from 'assets/images/GIF_01.gif'
+import gif02 from 'assets/images/GIF_02.gif'
+import image01 from 'assets/images/IMG_0012.jpg'
+import image02 from 'assets/images/IMG_0026.jpg'
+import image03 from 'assets/images/IMG_0043.jpg'
+import image04 from 'assets/images/IMG_0078.jpg'
+import image05 from 'assets/images/IMG_0123.jpg'
+import image06 from 'assets/images/IMG_0145.jpg'
+import image07 from 'assets/images/IMG_0154.jpg'
+import image08 from 'assets/images/IMG_0164.jpg'
+import image09 from 'assets/images/IMG_0181.jpg'
+import image10 from 'assets/images/IMG_0190.jpg'
+import image11 from 'assets/images/IMG_0220.jpg'
+import image12 from 'assets/images/IMG_0235.jpg'
+import image13 from 'assets/images/IMG_0269.jpg'
+import image14 from 'assets/images/IMG_0287.jpg'
+import image15 from 'assets/images/IMG_0337.jpg'
+import image16 from 'assets/images/IMG_0348.jpg'
+import image17 from 'assets/images/IMG_0374.jpg'
+import image18 from 'assets/images/IMG_0383.jpg'
+import image19 from 'assets/images/IMG_0415.jpg'
+import image20 from 'assets/images/IMG_0433.jpg'
+import image21 from 'assets/images/IMG_0441.jpg'
+import image22 from 'assets/images/IMG_0450.jpg'
+import image23 from 'assets/images/IMG_0521.jpg'
+import ImageGrid from 'components/ImageGrid'
+
 const Gallery = () => {
   return (
     <section>
       <h2>GALLERY</h2>
+      <ImageGrid
+        imageList={[
+          image01,
+          image02,
+          image03,
+          image04,
+          image05,
+          image06,
+          image07,
+          image08,
+          image09,
+          image10,
+          image11,
+          image12,
+          image13,
+          image14,
+          image15,
+          image16,
+          image17,
+          image18,
+          image19,
+          image20,
+          image21,
+          image22,
+          image23,
+          gif01,
+          gif02,
+        ]}
+      />
     </section>
   )
 }
