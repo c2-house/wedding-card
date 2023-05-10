1024cac3feba106ca3866b9db80657adbdd9d36f
eunchae <kec0130@gmail.com>
2023-05-10 23:01:34 +0900

    add navbar animation

--- src/components/Layout/GNB/NavBar.tsx
+++ src/components/Layout/GNB/NavBar.tsx
@@ -1,4 +1,5 @@
 import cx from 'classnames'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './gnb.module.scss'
@@ -27,18 +28,34 @@ const navList: INavItem[] = [
   },
 ]
 
+const ITEM_WIDTH = 100 / navList.length
+
 const NavBar = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
   return (
     <nav className={styles.navBar}>
       <ul>
-        {navList.map((item) => (
+        {navList.map((item, index) => (
           <li key={`gnb-item-${item.title}`}>
-            <NavLink to={item.to} className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+            <NavLink
+              to={item.to}
+              className={({ isActive }) => cx({ [styles.isActive]: isActive })}
+              onClick={() => setCurrentIndex(index)}
+              style={{ width: `${ITEM_WIDTH}%` }}
+            >
               {item.title}
             </NavLink>
           </li>
         ))}
       </ul>
+      <span
+        className={styles.indicator}
+        style={{
+          width: `${ITEM_WIDTH}%`,
+          transform: `translateX(${currentIndex * 100}%)`,
+        }}
+      />
     </nav>
   )
 }
