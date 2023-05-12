0ae3314bc2e2f24df29ada4ad1bd9eaea017860e
eunchae <kec0130@gmail.com>
2023-05-12 23:06:36 +0900

    add fade in animation to home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import cx from 'classnames'
 
 import Button from 'components/Button'
 import { useScrollTo } from 'hooks/useScrollTo'
@@ -12,23 +13,29 @@ const Home = () => {
     <div className={styles.homepage}>
       <div className={styles.banner}>
         <div className={styles.infoText}>
-          <div>
+          <div className={styles.fadeIn} style={{ animationDelay: '200ms' }}>
             신랑 <span>정민채</span> & 신부 <span>고은채</span>
           </div>
-          <p>
+          <p className={styles.fadeIn} style={{ animationDelay: '400ms' }}>
             2023. 6. 10 (토) 오후 4시
             <br />
             아펠가모 공덕 7층 마리에홀
           </p>
         </div>
       </div>
-      <p className={styles.inviteText}>
+      <p className={cx(styles.inviteText, styles.fadeIn)} style={{ animationDelay: '600ms' }}>
         민채와 은채의 결혼식에
         <br />
         소중한 분들을 초대합니다.
       </p>
       <div className={styles.buttonWrapper}>
-        <Button text='청첩장 보기' size='fullWidth' onClick={() => navigate('/invitation')} />
+        <Button
+          className={styles.fadeIn}
+          style={{ animationDelay: '800ms' }}
+          text='청첩장 보기'
+          size='fullWidth'
+          onClick={() => navigate('/invitation')}
+        />
       </div>
     </div>
   )
