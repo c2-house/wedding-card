1dec4ae67f6d511ced912999cf31489498ea8792
eunchae <kec0130@gmail.com>
2023-05-14 19:12:54 +0900

    update location section

--- src/routes/Invitation/components/Location.tsx
+++ src/routes/Invitation/components/Location.tsx
@@ -5,9 +5,23 @@ import ImageCarousel from 'components/ImageCarousel'
 import image1 from 'assets/images/weddinghall_01.jpeg'
 import image2 from 'assets/images/weddinghall_02.jpeg'
 import image3 from 'assets/images/weddinghall_03.jpeg'
-import image4 from 'assets/images/weddinghall_04.jpeg'
 import kakaoIcon from 'assets/icons/kakao_map.png'
 import naverIcon from 'assets/icons/naver_map.png'
+
+const addressText = [
+  {
+    title: '주소',
+    description: '서울특별시 마포구 마포대로 92\n효성해링턴스퀘어 B동 7층 마리에홀',
+  },
+  {
+    title: '오시는 길',
+    description: '공덕역 10번 출구 (경의중앙선, 공항철도)\n공덕역 7번 출구 (5호선, 6호선)',
+  },
+  {
+    title: '주차 안내',
+    description: '네비에 공덕역 10번 출구로 검색하시면\n주차장 입구 쪽으로 안내됩니다. (2시간 무료)',
+  },
+]
 
 const Location = () => {
   const mapElement = useRef<HTMLDivElement>(null)
@@ -15,9 +29,11 @@ const Location = () => {
   useEffect(() => {
     if (!mapElement.current) return
 
+    const center = new naver.maps.LatLng(37.5427105, 126.9524296)
     const location = new naver.maps.LatLng(37.5422, 126.9526)
+
     const mapOptions: naver.maps.MapOptions = {
-      center: location,
+      center,
       zoom: 16,
       zoomControl: false,
       disableDoubleTapZoom: true,
@@ -39,14 +55,15 @@ const Location = () => {
     <section>
       <h2>LOCATION</h2>
       <h3 className={styles.location}>아펠가모 공덕</h3>
-      <ImageCarousel images={[image1, image2, image3, image4]} indicatorPosition='outer' />
+      <ImageCarousel images={[image1, image2, image3]} indicatorPosition='outer' intervalTime={4000} delay={800} />
       <div ref={mapElement} className={styles.map} />
       <div className={styles.address}>
-        <p>
-          서울특별시 마포구 마포대로 92
-          <br />
-          효성해링턴스퀘어 B동 7층 마리에홀
-        </p>
+        {addressText.map((text) => (
+          <div key={text.title} className={styles.addressItem}>
+            <h4>{text.title}</h4>
+            <p>{text.description}</p>
+          </div>
+        ))}
         <div className={styles.mapIcons}>
           <a href='https://naver.me/GSUnQUY2' target='_blank' rel='noreferrer'>
             <img src={naverIcon} alt='네이버 지도' />
